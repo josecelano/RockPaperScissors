b30b0a30c50da8e28776a1def66ff35ef775ac72
Rubén Díaz Martínez <rubendm92@gmail.com>
2016-01-17 21:14:44 +0000

    Solve using ifs and primitives

---
+++ src/if_solution/RockPaperScissorsSolver.java
@@ -0,0 +1,67 @@
+package if_solution;
+
+public class RockPaperScissorsSolver {
+
+	public static final String ROCK = "rock";
+	public static final String PAPER = "paper";
+	public static final String SCISSORS = "scissors";
+
+	public static String solve(String leftGesture, String rightGesture) {
+		if (leftGesture.equals(rightGesture)) {
+			return "Tie.";
+		}
+		if (isRock(leftGesture)) {
+			return againstRock(rightGesture);
+		}
+		if (isPaper(leftGesture)) {
+			return againstPaper(rightGesture);
+		}
+		if (isScissors(leftGesture)) {
+			return againstScissors(rightGesture);
+		}
+		throw new UnsupportedOperationException("Invalid gesture.");
+	}
+
+	private static String againstRock(String gesture) {
+		if (isPaper(gesture)) {
+			return "Rock loses against paper.";
+		}
+		if (isScissors(gesture)) {
+			return "Rock wins against scissors.";
+		}
+		throw new UnsupportedOperationException("Invalid gesture.");
+	}
+
+	private static String againstPaper(String gesture) {
+		if (isRock(gesture)) {
+			return "Paper wins against rock.";
+		}
+		if (isScissors(gesture)) {
+			return "Paper loses against scissors.";
+		}
+		throw new UnsupportedOperationException("Invalid gesture.");
+	}
+
+
+	private static String againstScissors(String gesture) {
+		if (isRock(gesture)) {
+			return "Scissors lose against rock.";
+		}
+		if (isPaper(gesture)) {
+			return "Scissors win against paper.";
+		}
+		throw new UnsupportedOperationException("Invalid gesture.");
+	}
+
+	private static boolean isRock(String gesture) {
+		return gesture.equals(ROCK);
+	}
+
+	private static boolean isPaper(String gesture) {
+		return gesture.equals(PAPER);
+	}
+
+	private static boolean isScissors(String gesture) {
+		return gesture.equals(SCISSORS);
+	}
+}
---
+++ test/if_solution/RockPaperScissorSolverShould.java
@@ -0,0 +1,45 @@
+package if_solution;
+
+import org.junit.Test;
+
+import static if_solution.RockPaperScissorsSolver.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RockPaperScissorSolverShould {
+
+	@Test
+	public void tie_when_both_gestures_are_equals() {
+		assertThat(solve(ROCK, ROCK), is("Tie."));
+	}
+
+	@Test
+	public void say_rock_loses_against_paper() {
+		assertThat(solve(ROCK, PAPER), is("Rock loses against paper."));
+	}
+
+	@Test
+	public void say_rock_wins_against_scissors() {
+		assertThat(solve(ROCK, SCISSORS), is("Rock wins against scissors."));
+	}
+
+	@Test
+	public void say_paper_wins_against_rock() {
+		assertThat(solve(PAPER, ROCK), is("Paper wins against rock."));
+	}
+
+	@Test
+	public void say_paper_loses_against_scissors() {
+		assertThat(solve(PAPER, SCISSORS), is("Paper loses against scissors."));
+	}
+
+	@Test
+	public void say_scissors_lose_against_rock() {
+		assertThat(solve(SCISSORS, ROCK), is("Scissors lose against rock."));
+	}
+
+	@Test
+	public void say_scissors_win_against_paper() {
+		assertThat(solve(SCISSORS, PAPER), is("Scissors win against paper."));
+	}
+}
